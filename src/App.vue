2997a309d68495df15f31228c0698467555c3057
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-09-25 19:39:30 -0400

    deployment sept 25 meeting

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 			<router-link to="/" class="row1 col1"><h1>KALPANA MOHAN</h1></router-link>
 			<p class="row1 col2 tagline">writer.</p>
 			<router-link v-on:mouseover="active = !active" to="/books" class="row1 col3 work">BOOKS</router-link>
-			<router-link to="/work" class="row1 col6">WORK</router-link>
+			<router-link to="/work" class="row1 col4">WORK</router-link>
 			<router-link to="/travel" class="row1 col4">TRAVEL</router-link>
-			<router-link to="/about" class="row1 col5">ABOUT</router-link>
+			<router-link to="/about" class="row1 col6">ABOUT</router-link>
 		</div>
 		<hr>
 		<router-view class="row2 col123456"></router-view>
