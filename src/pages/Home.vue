128ed173bd755731eb8b59eeb0b6350bf3b2cb93
Parthiv Mohan <parthiv.mohan@gmail.com>
2022-01-09 17:58:47 -0500

    removed cors proxy

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -33,7 +33,7 @@
 			}
 		},
 		async created() {
-			let response = await fetch("https://cors-anywhen.herokuapp.com/" + this.$hostname + "/photos/location/home", { method: "GET", mode: 'cors', cache: 'no-cache', headers: { "X-Requested-With": "XMLHttpRequest" }});
+			let response = await fetch(this.$hostname + "/photos/location/home");
 			this.homeBanner = await response.json();
 			this.homeBanner = Buffer.from(this.homeBanner[0].img.data).toString('base64');
 			this.dataReady = true;
